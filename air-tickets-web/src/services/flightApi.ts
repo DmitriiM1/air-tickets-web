37967519d9aa4481b9874de7b29eda94efc0856c
Dmitrii Mitin <mda_200418@mail.ru>
2025-12-26 13:06:23 -0500

    add mock flight API service

---
+++ air-tickets-web/src/services/flightApi.ts
@@ -0,0 +1,17 @@
+import { Flight } from '../types/flight'
+
+const mockFlights: Flight[] = [
+  {
+    id: '1',
+    airline: 'Air Canada',
+    from: 'Toronto',
+    to: 'New York',
+    departureTime: '10:00',
+    arrivalTime: '12:00',
+    price: 320,
+  },
+]
+
+export function getFlights(): Promise<Flight[]> {
+  return Promise.resolve(mockFlights)
+}
