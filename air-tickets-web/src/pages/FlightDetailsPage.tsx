b007a37449e413a3ed5c4e82008b688203639d69
Dmitrii Mitin <mda_200418@mail.ru>
2025-12-26 13:28:27 -0500

    implement flight details page

--- air-tickets-web/src/pages/FlightDetailsPage.tsx
+++ air-tickets-web/src/pages/FlightDetailsPage.tsx
@@ -1,3 +1,27 @@
+import { useParams } from 'react-router-dom'
+import { getFlights } from '../services/flightApi'
+import { useEffect, useState } from 'react'
+import type { Flight } from '../types/flight'
+
 export default function FlightDetailsPage() {
-  return <h1>Flight Details</h1>
+  const { id } = useParams()
+  const [flight, setFlight] = useState<Flight | null>(null)
+
+  useEffect(() => {
+    getFlights().then(flights => {
+      setFlight(flights.find(f => f.id === id) || null)
+    })
+  }, [id])
+
+  if (!flight) return <p>Loading...</p>
+
+  return (
+    <>
+      <h1>{flight.airline}</h1>
+      <p>{flight.from} → {flight.to}</p>
+      <p>Departure: {flight.departureTime}</p>
+      <p>Arrival: {flight.arrivalTime}</p>
+      <p>Price: ${flight.price}</p>
+    </>
+  )
 }
