64f30d2b4e0c5530bde3059a186c0b75a362a3b3
Dmitrii Mitin <mda_200418@mail.ru>
2025-12-27 15:50:32 -0500

    prepare to deploy

--- air-tickets-web/src/App.tsx
+++ air-tickets-web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import SearchPage from './pages/SearchPage'
 import FlightDetailsPage from './pages/FlightDetailsPage'
 import Layout from './components/Layout'
@@ -6,14 +6,14 @@ import Layout from './components/Layout'
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/flight/:id" element={<FlightDetailsPage />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
